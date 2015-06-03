1f53cb51e32050b8b2476809a4847662d21159d3
Gabriel <gabriel.simoes@gmail.com>
2015-06-03 18:28:17 -0300

    03/06 - Backprop starts to learn at output layer. Implementation for hidden layers was started.

--- neural.py
+++ neural.py
@@ -2,29 +2,34 @@ __author__ = 'gabriel'
 
 import numpy as np
 import time
-import instance
-
+from instance import Instance
 
 class NeuralNet(object):
     """
     The implementation of MLP with back propagation learning algorithm.
     """
-    def __init__(self, input_values, architecture, weights = None):
-        self.x = input_values
-        self.sigma = 0.0
+    def __init__(self, architecture, weights = None):
+        self.learning_rate = 0.4
+        self.instance_data = Instance()
+        self.x = range(len(architecture))
         self.architecture = architecture
-        self.layers = list()
-        self.s = range(len(architecture) - 1)
-        self.z = range(len(architecture) - 1)
-        self.layers.append(input_values)
+        self.layers = range(len(architecture))
+        self.w_sum = range(len(architecture))
+        self.activation = range(len(architecture))
+        self.error = range(len(architecture))
         self.vsig = np.vectorize(NeuralNet.sigmoid)
+        self.dvsig = np.vectorize(NeuralNet.sigmoid_derivative)
+        np.random.seed(1)
         if weights is None:
-            self.theta = self.start_weights(architecture)
+            self.weights = NeuralNet.start_weights(architecture)
         else:
-            self.theta = weights
+            self.weights = weights
 
     def instances(self, inst):
-        self.instances_data = inst
+        self.instance_data = inst
+        self.layers[0] = inst.attributes
+        self.x[0] = inst.attributes
+        self.activation[0] = inst.attributes
 
     @staticmethod
     def start_weights(arch):
@@ -35,20 +40,31 @@ class NeuralNet(object):
 
     def feed_forward(self):
         for i in range(len(self.architecture) - 1):
-            tmp = (self.layers[i] * self.theta[i].T).sum(axis=0)
-            self.s[i] = tmp
-            tmp = self.vsig(tmp)
-            self.z[i] = tmp
-            tmp = tmp.reshape((tmp.shape[0], 1))
-            self.layers.append(tmp)
+            #tmp = (self.layers[i] * self.theta[i].T).sum(axis=0)
+            tmp = self.layers[i].T.dot(self.weights[i].T).T
+            self.w_sum[i + 1] = tmp
+            self.activation[i + 1] = self.vsig(tmp)
+            #self.error[i + 1] = self.dvsig(tmp)
+            # tmp = tmp.reshape((tmp.shape[0], 1))
+            self.layers[i + 1] = self.activation[i + 1]
 
     def back_propagate(self):
+        first = True
         for i in range(len(self.layers) - 1, -1, -1):
-            print "Camada..."
-            print self.layers[i]
-            if i > 0:
-                print "Pesos..."
-                print self.theta[i - 1]
+            out = self.activation[i]
+            if first:
+                # Output layer error calculation
+                term1 = np.ones(self.activation[i].shape) - out
+                term2 = (self.instance_data.output_values.T - out)
+                out_error = (term1 * term2) * out
+                # Output layer weights update
+                out = self.activation[i - 1]
+                self.weights[i - 1] = ((out_error.T * out * self.learning_rate) + self.weights[i - 1].T).T
+                first = False
+            else:
+                pass
+
+
 
     @staticmethod
     def signal(v):
@@ -75,33 +91,27 @@ class NeuralNet(object):
         return result
 
     def print_s(self):
-        for s in self.s:
+        for s in self.w_sum:
             print s
 
     def print_z(self):
-        for z in self.z:
+        for z in self.activation:
             print z
 
 def main():
-    x = np.array([[1], [1], [1], [0], [0], [0], [0], [1], [1], [0], [1], [1]])
-    theta = np.array([[[-1.3, 0.4, -0.6, -0.2, -1.3, 0.4, -0.6, -0.2, 0.9, 0.2, 0.3, 0.1]]])
-    # ann = NeuralNet(x, [12, 1], theta)
-    ann = NeuralNet(x, [12, 6, 5, 5, 5, 4, 1])
-    t = time.time() * 1000
-    ann.feed_forward()
-    # ann.back_propagate()
+    x = np.array([[0],[1],[1],[1],[0]])
+    y = np.array([[0, 1, 0]])
+    inst = Instance()
+    inst.attributes = x
+    inst.output_values = y
+    ann = NeuralNet([5,10,3])
+    ann.instances(inst)
 
-    ann.print_s()
-    print
-    ann.print_z()
-    # print
-    # print ann
+    for i in range(10):
+        ann.feed_forward()
+        ann.back_propagate()
 
-    print NeuralNet.sigmoid(-2)
-    print NeuralNet.sigmoid_derivative(-2)
+    print ann
 
-    # i = instance.Instance()
-    # t = (time.time() * 1000) - t
-    # print t
 if __name__ == "__main__":
     main()
